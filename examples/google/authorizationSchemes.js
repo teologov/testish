9f6a176347e066dc45790afe36dcc2d949f20eb3
Andrey Teologov <teologov.and@gmail.com>
2017-12-01 10:49:53 +0200

    add example of authorization scheme

---
+++ examples/google/authorizationSchemes.js
@@ -0,0 +1,11 @@
+/**
+ * An example of authorization schemes per environment
+ * @author Andrey Teologov <teologov.and@gmail.com>
+ */
+
+// eslint-disable-next-line
+module.exports = (driver, config) => ({
+  *production() {
+    // implement your log in scenario here
+  },
+});
